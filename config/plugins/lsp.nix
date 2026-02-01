b2d11ffba9b59b4e8d80ddecbf05c83e5ff2f70e
Archerymystery <ratibor200805@gmail.com>
2026-02-01 23:12:26 +0200

    configure lsp for esp32

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -32,6 +32,20 @@
       nixd = {
         enable = true;
       };
+      clangd = {
+        enable = true;
+        extraOptions = {
+          __raw = ''
+            (function()
+              local status_ok, esp32 = pcall(require, "esp32")
+              if os.getenv("ESP_MODE") == "1" and status_ok then
+                return esp32.lsp_config()
+              end
+             return {}
+            end)()
+          '';
+        };
+      };
     };
   };
 }
